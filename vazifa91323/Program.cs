f3c0a2c0ac9bcdde5208420569e7a59b8fe62e28
MuhammadumarMM <botirovmuhammadumar33.gmail.com>
2023-09-13 20:16:18 +0500

    Add project files.

---
+++ vazifa91323/Program.cs
@@ -0,0 +1,450 @@
+﻿// SWITCH
+
+////case 7
+//var tipp = int.Parse(Console.ReadLine());
+//switch (tipp)
+//{
+//    case 1:
+//        {
+//            var kilogramm = int.Parse(Console.ReadLine());
+//            Console.WriteLine(kilogramm + "kilogramm");
+//            break;
+//        }
+//    case 2:
+//        {
+//            var kilogramm = int.Parse(Console.ReadLine());
+//            Console.WriteLine($"{kilogramm / 10000}  kilogramm");
+//            break;
+//        }
+//    case 3:
+//        {
+//            var kilogramm = int.Parse(Console.ReadLine());
+//            Console.WriteLine($"{kilogramm / 1000}  kilogramm");
+//            break;
+//        }
+//    case 4:
+//        {
+//            var kilogramm = int.Parse(Console.ReadLine());
+//            Console.WriteLine($"{kilogramm * 1000}  kilogramm");
+//            break;
+//        }
+//    case 5:
+//        {
+
+//            var kilogramm = int.Parse(Console.ReadLine()); 
+//            Console.WriteLine($"{kilogramm * 100}   kilogramm");
+//            break;
+//        }
+
+//}
+
+// case 15
+//Console.Write("Iltimos 1 dan 4 gacha son kiriting :  ");
+//var tipp = int.Parse(Console.ReadLine());
+//switch (tipp)
+//{
+//    case 1:
+//        {
+//            Console.Write("iltimod 6 dan 14 gacha son kiriting : ");
+//            var qarga = int.Parse(Console.ReadLine());
+//            switch (qarga)
+//            {
+//                case 6:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 6 Qarg'a");
+//                        break;
+//                    }
+//                case 7:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 7 Qarg'a");
+//                        break;
+//                    }
+//                case 8:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 8 Qarg'a");
+//                        break;
+//                    }
+//                case 9:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 9 Qarg'a");
+//                        break;
+//                    }
+//                case 10:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 10 Qarg'a");
+//                        break;
+//                    }
+//                case 11:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Valet Qarg'a");
+//                        break;
+//                    }
+//                case 12:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Dama Qarg'a");
+//                        break;
+//                    }
+//                case 13:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Karol Qarg'a");
+//                        break;
+//                    }
+//                case 14:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Tuz Qarg'a");
+//                        break;
+//                    }
+//            }
+
+//            break;
+//        }
+//    case 2:
+//        {
+//            Console.Write("iltimod 6 dan 14 gacha son kiriting : ");
+//            var qarga = int.Parse(Console.ReadLine());
+//            switch (qarga)
+//            {
+//                case 6:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 6 olma");
+//                        break;
+//                    }
+//                case 7:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 7 olma");
+//                        break;
+//                    }
+//                case 8:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 8 olma");
+//                        break;
+//                    }
+//                case 9:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 9 olma");
+//                        break;
+//                    }
+//                case 10:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 10 olma");
+//                        break;
+//                    }
+//                case 11:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Valet olma");
+//                        break;
+//                    }
+//                case 12:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Dama olma");
+//                        break;
+//                    }
+//                case 13:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Karol olma");
+//                        break;
+//                    }
+//                case 14:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Tuz olma");
+//                        break;
+//                    }
+//            }
+
+//            break;
+//        }
+//    case 3:
+//        {
+//            Console.Write("iltimod 6 dan 14 gacha son kiriting : ");
+//            var qarga = int.Parse(Console.ReadLine());
+//            switch (qarga)
+//            {
+//                case 6:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 6 Chillik");
+//                        break;
+//                    }
+//                case 7:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 7 Chillik");
+//                        break;
+//                    }
+//                case 8:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 8 Chillik");
+//                        break;
+//                    }
+//                case 9:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 9 Chillik");
+//                        break;
+//                    }
+//                case 10:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 10 Chillik");
+//                        break;
+//                    }
+//                case 11:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Valet Chillik");
+//                        break;
+//                    }
+//                case 12:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Dama Chillik");
+//                        break;
+//                    }
+//                case 13:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Karol Chillik");
+//                        break;
+//                    }
+//                case 14:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Tuz Chillik");
+//                        break;
+//                    }
+//            }
+
+//            break;
+//        }
+//    case 4:
+//        {
+//            Console.Write("iltimod 6 dan 14 gacha son kiriting : ");
+//            var qarga = int.Parse(Console.ReadLine());
+//            switch (qarga)
+//            {
+//                case 6:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 6 G'ishtin");
+//                        break;
+//                    }
+//                case 7:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 7 G'ishtin");
+//                        break;
+//                    }
+//                case 8:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 8 G'ishtin");
+//                        break;
+//                    }
+//                case 9:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 9 G'ishtin");
+//                        break;
+//                    }
+//                case 10:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz 10 G'ishtin");
+//                        break;
+//                    }
+//                case 11:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Valet G'ishtin");
+//                        break;
+//                    }
+//                case 12:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Dama G'ishtin");
+//                        break;
+//                    }
+//                case 13:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Karol G'ishtin");
+//                        break;
+//                    }
+//                case 14:
+//                    {
+
+//                        Console.WriteLine("Tanlagan kartangiz Tuz G'ishtin");
+//                        break;
+//                    }
+//            }
+
+//            break;
+//        }
+//}
+
+
+
+//// While
+
+// While 4
+//var n = int.Parse(Console.ReadLine());
+//int sum = 0;
+//while (n > 3)
+//{
+//    n = n / 3;
+//    sum = n;
+//}
+//if (sum == 3)
+//    {
+//        Console.WriteLine("True");
+//    }
+//else
+//    {
+//        Console.WriteLine("False");
+//    }    
+
+
+
+//// FOr
+
+
+////   for 15
+//var n = int.Parse(Console.ReadLine());
+//double a = double.Parse(Console.ReadLine());
+//double summ = 1;
+//for (int i = 0; i < n; i++)
+//{
+//    summ *= a;
+//}
+//Console.WriteLine(summ);
+
+
+////   for 17
+//var n = int.Parse(Console.ReadLine());
+//double a = double.Parse(Console.ReadLine());
+//double summ = 1;
+//double count = 0;
+//for (int i = 0; i < n; i++)
+//{
+//    summ *= a;
+//    count += summ;
+//}
+//Console.WriteLine($"{count + 1}");
+
+
+//// for 19
+//var n = int.Parse(Console.ReadLine());
+//int sum = 1;
+//for  (int i = 1; i <= n; i++)
+//{
+//    sum *= i;
+//}
+//Console.WriteLine(sum);
+
+
+//// for 37
+//var n = int.Parse(Console.ReadLine());
+//var k  = int.Parse(Console.ReadLine());
+//double summ = 0;
+//for  (int i = 1; i <= n; i++)
+//{
+//    summ += Math.Pow(i , k);
+//}
+//Console.WriteLine(summ);
+
+
+
+////var n = int.Parse(Console.ReadLine());
+////var b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+////int A = b[0];
+////int B = b[1];
+////int C = b[2];
+////if (A + B + C >= n)
+////{
+////    Console.WriteLine("Yes");
+////}
+////else
+////{
+////    Console.WriteLine("No");
+////}
+
+////var n = int.Parse(Console.ReadLine());
+////int a;
+////if (n > 37)
+////{
+////    if (n % 5 > 2)
+////    {
+////        a = n / 5 + 1;
+////        Console.WriteLine($"{5 * a}");
+////    }
+////    else
+////    {
+////        Console.WriteLine(n);
+////    }
+////}
+////else
+////{
+////    Console.WriteLine(n);
+////}
+////var i = Console.ReadLine().Split().Select(int.Parse).ToArray();
+////int n = i[0];
+////int m  = i[1];
+////int x;
+////if (n / 2 >= m)
+////{
+////    x = n - 2 * m;
+////    Console.WriteLine(x);
+////}
+////else
+////{
+////    Console.WriteLine("-1");
+////}
+
+//var i = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int a = i[0];
+//int b = i[1];
+//int e = 0;
+//var k = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int[] sonlar = new int[100];
+//for  (int j = 0; j < a; j++)
+//{
+//    //if (k[j] == 0)
+//    //{
+//        e += j;
+//        Console.WriteLine(e);
+//    //}
+//}
+//string son = Console.ReadLine();
+//int juft_sum = 0;
+//int toq_sum = 0;
+//for (int i = 0; i < son.Length; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        juft_sum += int.Parse(son[i].ToString());
+//    }
+//    else
+//    {
+//        toq_sum += int.Parse(son[i].ToString());
+//    }
+//}
+//string res = toq_sum == juft_sum ? "Yes" : "No";
